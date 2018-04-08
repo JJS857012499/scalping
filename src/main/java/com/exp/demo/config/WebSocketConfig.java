b67d0468a90b1f4763332954f14642ee4e892f32
江俊升 <857012499@qq.com>
2018-04-08 15:50:02 +0800

    去除无用依赖

--- src/main/java/com/exp/demo/config/WebSocketConfig.java
+++ src/main/java/com/exp/demo/config/WebSocketConfig.java
@@ -1,18 +1,18 @@
-package com.exp.demo.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.server.standard.ServerEndpointExporter;
-
-/**
- * @author JianChow
- * @date 2018-04-01
- */
-@Configuration
-public class WebSocketConfig {
-    @Bean
-    public ServerEndpointExporter serverEndpointExporter() {
-        return new ServerEndpointExporter();
-    }
-
-}
+//package com.exp.demo.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+//
+///**
+// * @author JianChow
+// * @date 2018-04-01
+// */
+//@Configuration
+//public class WebSocketConfig {
+//    @Bean
+//    public ServerEndpointExporter serverEndpointExporter() {
+//        return new ServerEndpointExporter();
+//    }
+//
+//}
